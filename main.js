83e3036d8f33bc222cfab58de2d0e4a5a1ab00a3
Sofie <solianyksofie@gmail.com>
2024-08-05 16:57:23 +0200

    task 1

---
+++ main.js
@@ -0,0 +1,12 @@
+let someNumber, someText, someBool, someBigNum;
+let arrayOfNumbers = [5, 65, 100, 2, 12];
+someNumber = 523;
+someText = "Hi!";
+someBool = true;
+someBigNum = 5451525123165151n;
+
+console.log(typeof someNumber);
+console.log(typeof someText);
+console.log(typeof someBool);
+console.log(typeof someBigNum); 
+console.log(typeof arrayOfNumbers);
