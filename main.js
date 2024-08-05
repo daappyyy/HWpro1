849b2a1e8289030d4c16f1666ce86edd4527b0c0
Sofie <solianyksofie@gmail.com>
2024-08-05 17:20:35 +0200

    Task 2

--- main.js
+++ main.js
@@ -1,12 +1,32 @@
-let someNumber, someText, someBool, someBigNum;
-let arrayOfNumbers = [5, 65, 100, 2, 12];
-someNumber = 523;
-someText = "Hi!";
-someBool = true;
-someBigNum = 5451525123165151n;
 
-console.log(typeof someNumber);
-console.log(typeof someText);
-console.log(typeof someBool);
-console.log(typeof someBigNum); 
-console.log(typeof arrayOfNumbers);
+//------------------Task 1-------------------------------
+
+// let someNumber, someText, someBool, someBigNum;
+// let arrayOfNumbers = [5, 65, 100, 2, 12];
+// someNumber = 523;
+// someText = "Hi!";
+// someBool = true;
+// someBigNum = 5451525123165151n;
+
+// console.log(typeof someNumber);
+// console.log(typeof someText);
+// console.log(typeof someBool);
+// console.log(typeof someBigNum); 
+// console.log(typeof arrayOfNumbers);
+
+
+
+// ------------------Task 2--------------------------------
+
+// let firstLine = prompt("Enter the first word, please");
+// let secondLine =  prompt("Enter the second word, please");
+// let thirdLine = prompt("Enter the third word, please");
+
+// document.write(secondLine + ' ', firstLine + ' ', thirdLine + ' ');
+
+
+// ------------------Task 3--------------------------------
+
+// let number = prompt("Enter a number which consists of 5 digits :)");
+
+// document.write(number[0] + ' ', number[1] + ' ', number[2] + ' ', number[3] + ' ', number[4] + ' ');
